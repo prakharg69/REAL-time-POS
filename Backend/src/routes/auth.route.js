7f8ba4df59f7ea834a238857eedcfc12bdb5cc2c
Prakhar Gussain <prakhargussain069@gmail.com>
2026-05-03 21:32:06 +0530

    feat(auth): add /logout route with logout controller

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { Login, SignUp } from "../controller/auth.controller.js";
+import { Login, logOut, SignUp } from "../controller/auth.controller.js";
 
 const AuthRoute = express.Router();
 
 AuthRoute.post("/Signup",SignUp);
 AuthRoute.post("/login",Login);
+AuthRoute.get("/logout",logOut);
 
 export default AuthRoute;
